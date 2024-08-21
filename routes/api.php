a48059f8a0b1a2e875e3790a7841cd11328bb032
Nicodav28 <esteveznicolas0@gmail.com>
2024-08-21 10:11:19 -0500

    📝 (api.php): reorganizar las rutas y añadir nuevas rutas para usuarios, pagos y reservas Se reorganizaron las rutas para mejorar la legibilidad y mantenibilidad del código. Se añadieron nuevas rutas para las operaciones relacionadas con los usuarios, los pagos y las reservas, lo que permite una mejor estructuración y separación de responsabilidades en la aplicación.

--- routes/api.php
+++ routes/api.php
@@ -5,25 +5,55 @@ use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-    $dateTime = DB::select('SELECT CURRENT_TIMESTAMP AS current_timestamp');
-    return "Hello world, today is " . $dateTime[0]->current_timestamp;
-});
+Route::group([ 'middleware' => [ 'trimHeaders', 'jwtAuth', 'basicAuth' ] ], function () {
+    Route::get('/', function () {
+        $dateTime = DB::select('SELECT CURRENT_TIMESTAMP AS current_timestamp');
+        return "Hello world, today is " . $dateTime[0]->current_timestamp;
+    })->withoutMiddleware([ 'jwtAuth', 'basicAuth' ]);
 
-Route::group([ 'prefix' => 'consumer' ], function () {
-    Route::post('/', [ APIConsumerController::class, 'createAPIConsumer' ])->name('createAPIConsumer');
-});
-
-Route::group([ 'prefix' => 'reservation', 'middleware' => [ 'trimHeaders', 'jwtAuth', 'basicAuth' ] ], function () {
-    Route::get('/jwt', function () {
-        return "Hey with JWT Auth Middleware";
-    })->withoutMiddleware('basicAuth');
-
-    Route::get('/basic', function () {
-        return "Hey with Basic Auth Middleware";
-    })->withoutMiddleware('jwtAuth');
+    Route::group([ 'prefix' => 'consumer' ], function () {
+        Route::post('/', [ APIConsumerController::class, 'createAPIConsumer' ])->name('createAPIConsumer');
+    });
 
     Route::group([ 'prefix' => 'restaurant' ], function () {
         Route::post('/', [ RestaurantController::class, 'createRestaurant' ])->name('createRestaurant')->withoutMiddleware([ 'jwtAuth', 'basicAuth' ]);
+
+        Route::group([ 'prefix' => 'user' ], function () {
+            Route::post('/', function () {
+                return "Create User";
+            });
+
+            Route::get('/{id}', function () {
+                return "Find User";
+            });
+
+            Route::get('/', function () {
+                return "Fetch Users";
+            });
+
+            Route::put('/{id}', function () {
+                return "Update User";
+            });
+
+            Route::delete('/{id}', function () {
+                return "Delete User";
+            });
+        });
+    })->withoutMiddleware([ 'jwtAuth', 'basicAuth' ]);
+
+    Route::group([ 'prefix' => 'payment' ], function () {
+        Route::post('/', function () {
+            return "payment";
+        });
+    });
+
+    Route::group([ 'prefix' => 'reservation' ], function () {
+        Route::get('/jwt', function () {
+            return "Hey with JWT Auth Middleware";
+        })->withoutMiddleware('basicAuth');
+
+        Route::get('/basic', function () {
+            return "Hey with Basic Auth Middleware";
+        })->withoutMiddleware('jwtAuth');
     });
 });
